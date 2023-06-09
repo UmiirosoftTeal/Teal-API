be97375cd0957f34c7d3917180467357d5f3363f
gamma-410 <mail@gamma410.win>
2023-06-10 00:56:34 +0900

    userのDB処理と機能追加

--- main.py
+++ main.py
@@ -12,41 +12,63 @@ tweet = db["tweet"]
 def viewAllTweet():
     return {"message": "view!"}
 
+
 @app.get("/view/{user_id}/{tweet_id}")
 def viewTweet(user_id: str, tweet_id: int):
     return {"user_id": user_id, "tweet_id": tweet_id}
 
+
 @app.get("/user/{user_id}")
 def userProfile(user_id: str):
-    userData = user.find(userid=user_id)
-    return {"userData": userData}
+    try:
+        userData = user.find_one(userid=user_id)
+        return {"userData": userData}
+    except:
+        return {"result": "None"}
+
 
 @app.post("/signup")
 def signUp(email: str, password: str, userid: str):
     try:
-        user.insert({"email": email, "password": password, "userid": userid, "detail": None})
-        return {"result": "success"}
+        user.insert({"email": email, "password": password,
+                    "userid": userid, "detail": ""})
+        return {"result": "POST"}
     except:
-        return {"result": "error"}
+        return {"result": "None"}
+
 
 @app.post("/new")
 def newTweet(email: str, password: str, tweet: str):
     return {"email": email, "password": password, "tweet": tweet}
 
+
 @app.post("/reply/{user_id}/{tweet_id}")
 def replyTweet(user_id: str, tweet_id: int, email: str, password: str, reply: str):
     return {"user_id": user_id, "tweet_id": tweet_id, "email": email, "password": password, "reply": reply}
 
+
 @app.post("/user/{user_id}")
 def changeProfile(user_id: str, email: str, password: str, detail: str):
-    return {"user_id": user_id, "email": email, "password": password, "detail": detail}
+    try:
+        userData = user.find_one(email=email)
+        if userData["password"] == password:
+            user.update({"userid": user_id, "detail": detail}, ["userid"])
+            return {"result": "POST"}
+    except:
+        return {"result": "None"}
+
 
 @app.post("/delete/tweet/{user_id}/{tweet_id}")
 def deleteTweet(user_id: str, tweet_id: int, email: str, password: str):
     return {"user_id": user_id, "tweet_id": tweet_id, "email": email, "password": password}
 
+
 @app.post("/delete/user/{user_id}")
 def deleteUser(user_id: str, email: str, password: str):
-    return {"user_id": user_id, "email": email, "password": password}
-
-
+    try:
+        userData = user.find_one(email=email)
+        if userData["password"] == password:
+            user.delete(userid=user_id)
+            return {"result": "POST"}
+    except:
+        return {"result": "None"}
